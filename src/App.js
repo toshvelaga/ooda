48d4806edaf213b0ecd69dd1a3ae47855a65cddb
Tosh Velaga <toshvelaga@gmail.com>
2020-11-15 22:44:20 -0800

    added folder structure and react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Search from "./containers/Search/Search";
+import ProviderDetail from "./containers/ProviderDetail/ProviderDetail";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<Switch>
+					<Route path="/provider/:id">
+						<ProviderDetail />
+					</Route>
+					<Route path="/">
+						<Search />
+					</Route>
+				</Switch>
+			</Router>
+		</>
+	);
 }
 
 export default App;
